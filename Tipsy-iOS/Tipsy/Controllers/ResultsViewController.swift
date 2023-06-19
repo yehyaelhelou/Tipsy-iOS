fd6a5702a058d32e55dfa38c55aae00da5d4517e
yehyaelhelou <95245462+yehyaelhelou@users.noreply.github.com>
2023-06-19 17:40:15 +0300

    Add files via upload

---
+++ Tipsy-iOS/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by yehya elhelou on 19/6/2023.
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
